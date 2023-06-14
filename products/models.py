5946805c4d4812354f13156394c68b00a3803eb4
Given <moretigiven@gmail.com>
2023-06-14 21:25:20 +0200

    make UI/UX customs

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from . import views 
+
 from django.contrib.auth.models import User
 from datetime import datetime
 # Create your models here.
@@ -8,19 +8,24 @@ from datetime import datetime
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.name}"
+
 class Product(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
     quantity = models.PositiveIntegerField()
-    # image = models.CharField(max_length=8000)
+    image = models.CharField(max_length=8000)
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     date_added = models.DateTimeField(auto_now_add=True)# include timesence in index.html
     date_edited = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.name}"
-
+    
+    class Meta:
+        ordering = ['-date_added']
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
--- products/views.py
+++ products/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Product
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404,render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login ,logout
+
 
 # Create your views here.
 def home(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products':products}
     return render(request,"products/index.html",context)
+
