2b82936bed0d959cb63f311ce0005bc65d72aacb
Given <moretigiven@gmail.com>
2023-07-07 21:19:16 +0200

    add products form

--- products/forms.py
+++ products/forms.py
@@ -1,7 +1,9 @@
 from django import forms
-from .models import Sell,UserProfile
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from .models import Sell, UserProfile, Product
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+
+
 class SearchForm(forms.Form):
     query = forms.CharField(max_length=100)
 
@@ -18,6 +20,8 @@ class UserProfileForm(forms.ModelForm):
         fields = "__all__"
 
 # user registrations form
+
+
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
@@ -26,24 +30,23 @@ class RegistrationForm(UserCreationForm):
         fields = ('username', 'email', 'password1', 'password2')
 
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder':"username",
-        'class':'w-full py-4 px-6 rounded-xl'
+        'placeholder': "username",
+        'class': 'w-full py-4 px-6 rounded-xl'
     }))
 
     email = forms.CharField(widget=forms.EmailInput(attrs={
-        'placeholder':"Email",
-        'class':'w-full py-4 px-6 rounded-xl'
+        'placeholder': "Email",
+        'class': 'w-full py-4 px-6 rounded-xl'
     }))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder':"Enter password",
-        'class':'w-full py-4 px-6 rounded-xl'
+        'placeholder': "Enter password",
+        'class': 'w-full py-4 px-6 rounded-xl'
     }))
 
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder':"Confirm password",
-        'class':'w-full py-4 px-6 rounded-xl'
+        'placeholder': "Confirm password",
+        'class': 'w-full py-4 px-6 rounded-xl'
     }))
-
 
     def save(self, commit=True):
         user = super(RegistrationForm, self).save(commit=False)
@@ -51,17 +54,25 @@ class RegistrationForm(UserCreationForm):
         if commit:
             user.save()
         return user
-    
 
 
 # user login form.
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder':"username",
-        'class':'w-full py-4 px-6 rounded-xl'
+        'placeholder': "username",
+        'class': 'w-full py-4 px-6 rounded-xl'
     }))
 
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder':"Enter password",
-        'class':'w-full py-4 px-6 rounded-xl'
+        'placeholder': "Enter password",
+        'class': 'w-full py-4 px-6 rounded-xl'
     }))
+
+# add a new product form.
+
+
+class AddNewProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ('name', 'description', 'price',
+                  'quantity', 'image', 'category','sold_by',)
--- products/views.py
+++ products/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from .forms import SearchForm, SellNowForm, UserProfileForm
+from .forms import AddNewProductForm, SearchForm, SellNowForm, UserProfileForm
 from .forms import RegistrationForm
 # Create your views here.
 
@@ -24,6 +24,8 @@ def home(request):
     return render(request, "products/index.html", context)
 
 # when you click on an item it must open the details page of that item.
+
+
 def details(request, pk):
     product_details = get_object_or_404(Product, pk=pk)
     # related items to be displayed on details page where category is same
@@ -76,7 +78,7 @@ def login_user(request):
 
     return render(request, 'products/login.html')
 
-
+@login_required
 def logout_user(request):
     logout(request)
     return redirect("home")
@@ -138,3 +140,9 @@ def about(request):
     about = About.objects.all()
 
     return render(request, "products/about.html", {"about": about})
+
+
+@login_required
+def add_new_products(request):
+    form = AddNewProductForm()
+    return render(request, "products/add_new.html", {"form": form})
